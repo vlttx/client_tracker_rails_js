c39f98c39c85b0797846d1a6458e1a7f206641b7
VBTX <jv.svedaite@gmail.com>
2019-05-14 19:02:11 -0500

    add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+	class SessionsController < ApplicationController
+	def create
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+    session[:user_id] = @user.id
+ 
+    render 'application/home'
+  end
+ 
+  private
+ 
+  def auth
+    request.env['omniauth.auth']
+  end
+end
+
+end
