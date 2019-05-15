95571892f27dc130b861452f33dc181e60615d42
VBTX <jv.svedaite@gmail.com>
2019-05-15 15:07:31 -0500

    add user relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+	has_many :invoices
+	has_many :clients, through: :invoices
+	has_many :projects, through: :clients
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
