71ed8217be7e862a31f19ccb22dacf47e9d1f13e
VBTX <jv.svedaite@gmail.com>
2019-05-15 15:08:20 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
 	has_many :projects, through: :clients
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    validates :name, presence: true
+	validates :email, presence: true
+	validates :email, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
