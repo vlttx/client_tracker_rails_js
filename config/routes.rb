3b09b6b5273818016d4e48a87b97bbb57deb9379
VBTX <jv.svedaite@gmail.com>
2019-05-16 16:48:09 -0500

    add nested route to projects

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get '/auth/facebook/callback' => 'sessions#create'
   root to: "application#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :clients do
+  	resources :projects
+  end
 end
