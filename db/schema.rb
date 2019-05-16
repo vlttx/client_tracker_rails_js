5144e88ece6f1bbf2ed49445ce3c9d89870cd023
VBTX <jv.svedaite@gmail.com>
2019-05-16 16:48:32 -0500

    add website to client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_202258) do
+ActiveRecord::Schema.define(version: 2019_05_16_210551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_05_16_202258) do
     t.datetime "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "website"
   end
 
   create_table "users", force: :cascade do |t|
