c1741305f666a80d24c37999c20ce4f0a77f1a13
VBTX <jv.svedaite@gmail.com>
2019-05-16 16:49:42 -0500

    add actions to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
 	def index
 		@projects = current_user.projects
+		byebug
 	end
 
 	def show
@@ -11,6 +12,8 @@ class ProjectsController < ApplicationController
 		@project = Project.new
 		@project.type = params[:type]
 		@project.title = params[:title]
+		@projects.deadline = params[:deadline]
+		@project.save
 
 	end
 
