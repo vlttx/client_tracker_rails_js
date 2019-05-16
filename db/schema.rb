d796be0cc8a75412c1d843d6caf2e83b2a474db9
VBTX <jv.svedaite@gmail.com>
2019-05-16 15:26:11 -0500

    change column name in projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_203723) do
+ActiveRecord::Schema.define(version: 2019_05_16_202258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_203723) do
 
   create_table "projects", force: :cascade do |t|
     t.string "type"
-    t.string "name"
+    t.string "title"
     t.datetime "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
