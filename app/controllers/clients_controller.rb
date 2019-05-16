cc5c54f59135d7661cfb6e2fa872fb3aa0d3dcad
VBTX <jv.svedaite@gmail.com>
2019-05-16 16:49:16 -0500

    add actions to clients_controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,33 @@
 class ClientsController < ApplicationController
+	def index
+		@clients = current_user.clients
+	end
+
+	def show
+		@client = Client.find(params[:id])
+	end
+
+	def create
+		@client = Client.new(client_params)
+		@client.save
+		redirect_to client_path(@client)
+	end
+
+	def new
+		@client = Client.new
+	end
+
+	def update
+		@client = Client.find(params[:id])
+		@client.update(client_params)
+		redirect_to client_path(@client)
+	end
+
+	private
+
+	def client_params
+		params.require(:client).permit!
+	end
+
+
 end
