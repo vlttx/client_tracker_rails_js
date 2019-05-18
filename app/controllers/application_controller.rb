ae0a36de6f8a781e05b6f9e827e26e17c0a27b7b
VBTX <jv.svedaite@gmail.com>
2019-05-17 19:35:15 -0500

    add user_path instead of root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-	  before_action :authenticate_user!
 	  protect_from_forgery with: :exception
-
+	  before_action :authenticate_user!
+	  helper_method :current_user, :user_signed_in?, :user_session, :logged_in?, :destroy_user_session_path
 	def home
 	end
+
+	def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || user_path(resource)
+	end
+
 end
 
 
