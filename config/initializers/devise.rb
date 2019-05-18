70b9f89f52542bd1c9d6fce9cfa7010f0b9cdd55
VBTX <jv.svedaite@gmail.com>
2019-05-17 19:41:32 -0500

    add extra info fields

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,9 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+    config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email, name'
+  #   , {
+  # strategy_class: OmniAuth::Strategies::Facebook, 
+  # provider_ignores_state: true}
 end
