37d84e4013bc86d869058f87fdf06da18f8ed3ec
VBTX <jv.svedaite@gmail.com>
2019-05-17 19:42:31 -0500

    add omniauth_callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :projects
   resources :invoices
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: 'registrations' }
   resources :clients
-  get '/auth/facebook/callback' => 'sessions#create'
+  resources :users
   root to: "application#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :clients do
