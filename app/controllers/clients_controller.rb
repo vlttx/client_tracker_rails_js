0da6d87a0296b8234f4bee13c6d8869112cca2a4
VBTX <jv.svedaite@gmail.com>
2019-05-17 19:35:53 -0500

    fix client not being saved

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,20 +1,25 @@
 class ClientsController < ApplicationController
+	before_action :set_user
+
+
 	def index
 		@clients = current_user.clients
+
 	end
 
 	def show
 		@client = Client.find(params[:id])
 	end
 
-	def create
-		@client = Client.new(client_params)
-		@client.save
-		redirect_to client_path(@client)
-	end
-
 	def new
 		@client = Client.new
+	end
+
+	def create
+		@client = Client.new(client_params)
+		@client.user = current_user
+		@client.save
+
 	end
 
 	def update
@@ -27,6 +32,14 @@ class ClientsController < ApplicationController
 
 	def client_params
 		params.require(:client).permit!
+	end
+
+	def set_user
+		if current_user
+			@user = current_user
+		else
+			redirect_to new_user_registration
+		end
 	end
 
 
