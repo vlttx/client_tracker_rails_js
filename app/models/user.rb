a521d38fbd047a07e5d72b71ce657b4c85201005
VBTX <jv.svedaite@gmail.com>
2019-05-17 19:37:35 -0500

    add show action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ApplicationRecord
+	has_many :clients
 	has_many :invoices
-	has_many :clients, through: :invoices
 	has_many :projects, through: :clients
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    validates :name, presence: true
+  validates :name, presence: true
 	validates :email, presence: true
 	validates :email, uniqueness: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  	devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.name = auth.info.name
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,10]
+      end
+    end
 end
