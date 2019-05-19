f850ee72dba507d8b0a0346873ab1b4056dc8bb8
VBTX <jv.svedaite@gmail.com>
2019-05-18 20:55:15 -0500

    add nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  resources :projects
   resources :invoices
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: 'registrations' }
   resources :clients
-  resources :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: 'registrations' }
+  resources :clients do
+    resources :projects
+  end
   root to: "application#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :clients do
-  	resources :projects
+  resources :projects do
+    resources :clients
+  end
+  resources :users do 
+    resources :clients 
   end
 end
