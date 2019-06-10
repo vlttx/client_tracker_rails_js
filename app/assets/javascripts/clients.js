9acb230f2121273a5c5b9c3e4e616299ae95d9a4
VBTX <jv.svedaite@gmail.com>
2019-06-10 13:37:35 -0500

    add client constructor and prototype and new form

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -1,15 +1,72 @@
-<script type="text/javascript" chartset="utf-8">
+// <script type="text/javascript" chartset="utf-8">
+// $(function(){
+// 	$(".js-more").on("click", function(){
+// 		let id = $(this).data("id");
+// 		$.get("/clients/" + id + "/projects", function(project){
+// 			let projects_list = "<strong>Has projects</strong>";
+// 			if (projects === 'false'){
+// 				projects_list = "<strong>No projects yet</strong>";
+// 			}
+// 		});
+// 	})
+// };
+// doc loading:
 $(function(){
-	$(".js-more").on("click", function(){
-		let id = $(this).data("id");
-		$.get("/clients/" + id + "/projects", function(project){
-			let projects_list = "<strong>Has projects</strong>";
-			if (projects === 'false'){
-				projects_list = "<strong>No projects yet</strong>";
-			}
-		});
+	listenForClick()
+});
+
+function listenForClick(){
+	$('button.js-more').on('click', function(e){
+		e.preventDefault()
+		getClients()
 	})
-};
+}
+
+function getClients(){
+	$.ajax({
+		url: 'http://localhost:3000/clients',
+		method: 'get',
+		dataType: 'json'
+	}).done(function(data)){
+		let myClient = new Client(data[0])
+		let myClientHTML = myClient.clientHTML()
+		document.getElementById('ajax-posts').innerHTML += myClientHTML
+	})
+}
+
+
+class Client {
+	constructor(obj) {
+		this.id = obj.id
+		this.business_name = obj.business_name
+		this.address = obj.address
+		this.email = email
+		this.projects = projects
+	}
+}
+
+Client.prototype.clientHTML = function(){
+	return (`
+		<div>
+		<h3>${this.business_name}</h3>
+		<p><h4>${this.address}<h4></p>
+		<p><h4>${this.email}</h4></p>
+		<p><h4>${this.projects}</h4></p>
+		</div>
+		`)
+}
+
+Client.prototype.newClientForm = function(){
+	return (`
+		<strong>New Client Form</strong>
+		<form>
+		<input id='client-business-name' type='text' name='business-name'</input><br>
+		<input type='text' name='address'></input><br>
+		<input type='text' name='email'</input><br>
+		<input type='submit' />
+		</form>
+		`)
+}
 
 
 // document.addEventListener("turbolinks:load", function(){
