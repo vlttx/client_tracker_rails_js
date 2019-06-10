d2ac7e7d47af05cb5186234fd7ba281bf1f691fe
VBTX <jv.svedaite@gmail.com>
2019-06-10 10:57:51 -0500

    add function for fetching

---
+++ app/assets/javascripts/clients.js
@@ -0,0 +1,12 @@
+// <script type="text/javascript" chartset="utf-8">
+// $(function(){
+// 	$(".js-more").on("click", function(){
+// 		let id = $(this).data("id");
+// 		$.get("/clients/" + id + "/projects", function(project){
+// 			let projects_list = "<strong>Has projects</strong>";
+// 			if (projects === 'false'){
+// 				projects_list = "<strong>No projects yet</strong>";
+// 			}
+// 		});
+// 	})
+// };
