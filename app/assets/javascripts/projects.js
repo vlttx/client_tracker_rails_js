e34ab4d0cca1568cc0120002fb4736e5549cb750
VBTX <jv.svedaite@gmail.com>
2019-06-13 12:20:43 -0500

    add button to show index via JS

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,19 @@
 $(document).ready(()=>{
-    console.log('Here I am in projects')
+	console.log('Here I am in projects');
 });
+
+function listenForClick(){
+	$('button.js-proj').on('click', function(e){
+		// e.preventDefault()
+		console.log(e)
+		// let clientId = e.currentTarget.dataset.id
+		// fetch(`http://localhost:3000/clients/${clientId}.json`)
+	 // 	.then(response => response.json())
+		// .then(data => {
+		// const show = document.getElementById("client-show")
+		// let client = displayClient(data);
+		// show.innerHTML = client;
+		// console.log(show);
+		// });
+	})
+}
