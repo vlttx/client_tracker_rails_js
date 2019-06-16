f9c14bcd4a50551f896d8826227c7cde79acad8a
VBTX <jv.svedaite@gmail.com>
2019-06-16 14:04:39 -0500

    update listener

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,13 +2,55 @@ $(document).ready(()=>{
 	console.log('Here I am in projects');
 	listenForClicks()
 });
+
+// function listenForClicks(){
+// 		$('button.js-proj').on('click', function(e){
+// 			if (show === ''){
+// 				let projectId = e.currentTarget.dataset.id
+// 				fetch(`http://localhost:3000/projects/${projectId}.json`)
+// 				.then(response => response.json())
+// 				.then(data => {
+// 					const invoiceArr = data.invoices;
+// 					invoiceArr.forEach(function (invoice, index) {
+// 					const show = document.getElementById("invoices-show")
+// 					let iDate = invoice.created_at.slice(0,10);
+// 						show.innerHTML += `<p>Title: ${invoice.service}; Total: ${invoice.total}; Created: ${iDate}</p>`;
+// 				});
+//  					});
+// 			}else{
+// 					document.getElementById("invoices-show").remove()
+// 	}
+// });
+// }
+
+
+
 	let counter = 0
 	function listenForClicks(){
 	$('button.js-proj').on('click', function(e){
 			
 
 				if (counter === 0){
- 						let projectId = e.currentTarget.dataset.id
+					let projectId = e.currentTarget.dataset.id
+	fetch(`http://localhost:3000/projects/${projectId}.json`)
+	.then(response => response.json())
+	.then(data => {
+
+		const invoiceArr = data.invoices;
+		invoiceArr.forEach(function (invoice, index) {
+		const show = document.getElementById("invoices-show")
+		let iDate = invoice.created_at.slice(0,10);
+			show.innerHTML += `<p>Title: ${invoice.service}; Total: ${invoice.total}; Created: ${iDate}</p>`;
+		});;
+		return counter+=1;
+	});
+ 					
+				}else if (counter%2 != 0){
+					document.getElementById("invoices-show").remove()
+					return counter+=1;
+
+				}else{
+				let projectId = e.currentTarget.dataset.id
  						fetch(`http://localhost:3000/projects/${projectId}.json`)
 						.then(response => response.json())
 						.then(data => {
@@ -21,10 +63,7 @@ $(document).ready(()=>{
 									});;
 									return counter+=1;
 						});
-
-				}else{
-					alert('The list has already loaded!');
 				};
+
 		});
 	};
-
