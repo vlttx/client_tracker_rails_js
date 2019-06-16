7ad02171b63931da1f1ca3de5f95fcd4b4acea51
VBTX <jv.svedaite@gmail.com>
2019-06-16 14:06:48 -0500

    add client info

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,5 +1,19 @@
 module InvoicesHelper
 def last_modified(invoice)
-    invoice.updated_at.strftime("Last modified %A, %b %e, at %l:%M %p")
+    invoice.updated_at.strftime("last modified %b %e, at %l:%M %p")
+end
+
+def who_client(invoice)
+	if (invoice.project_id)
+		@project = Project.find(invoice.project_id)
+		@clients = current_user.clients
+		@clients.each do |c|
+			if c.projects.include?(@project)
+				return c.business_name
+			end
+		end
+	else
+	 return "no associated client"
+	end
 end
 end
