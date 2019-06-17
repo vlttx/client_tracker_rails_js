6f3cc193456662d3561a012888317ae9b287e6d0
VBTX <jv.svedaite@gmail.com>
2019-06-16 20:00:44 -0500

    add close button js

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -1,12 +1,17 @@
 
 
 $(document).ready(()=>{
+	let counter = 0;
 	console.log('Here I am in clients')
-	listenForClick()
-	listenForNewClientFormClick()
+	listenForClick();
+	listenForNewClientFormClick();
+	hideMe();
+	// cancelForm();
+
 });
 
 function listenForClick(){
+	if (counter===0){
 	$('button.js-more').on('click', function(e){
 		let clientId = e.currentTarget.dataset.id
 		fetch(`http://localhost:3000/clients/${clientId}.json`)
@@ -15,12 +20,35 @@ function listenForClick(){
 		const show = document.getElementById("client-show")
 		const client = new Client(data)
 		show.innerHTML = client.clientHTML();
-		});
+	
 	});
+		counter+=1
+	});
+	}else{
+		$('button.js-more').on('click', function(e){
+		const show = document.getElementById("client-show")
+		show.innerHTML = '';
+		
+	});
+		counter -=1;
+}
 }
 
 
+function hideMe(){
+	$('#client-show').on('click', function(e){
+			document.getElementById("client-show").innerHTML = ''
+	 });
+}
 
+// function cancelForm(){
+// 	console.log("Cancel is here")
+// 	$('#cancelMe').on('click', function(e){
+// 		console.log(e)
+// 			document.getElementById("newc").innerHTML = ''
+// 			document.getElementById("cancel").remove();
+// 	 });
+// }
 
 
 class Client {
@@ -34,14 +62,17 @@ class Client {
 
 		static newClientForm() {
 		return (`
+		<form id="newc" style="display:inline-block">
 		<strong>New Client Form</strong><br>
-		<form id="newc">
-		<input id='client-business-name' type='text' name="client[business_name]" placeholder = "Business Name"></input><br>
-		<input type='text' name="client[address]" placeholder = "Address"></input><br>
-		<input type='text' name="client[website]" placeholder = "Website"</input><br>
+		<input id='client-business-name' type='text' name="client[business_name]" placeholder = "Business Name*" required></input>
+		<input type='text' name="client[address]" placeholder = "Address"></input>
+		<input type='text' name="client[website]" placeholder = "Website"</input>
 		<input type='text' name="client[email]" placeholder = "Email"</input><br>
-		<input type='submit' />
+		<input type='submit' value= "SUBMIT" class="btn btn-success btn-md" />
+		<button type="button" id="cancelMe" class="btn btn-danger btn-md" style="display:inline-block">CANCEL</button>
 		</form>
+		<br>
+		
 		`)
 }
 		}
@@ -62,19 +93,23 @@ class Client {
 Client.prototype.clientHTML = function(){
 	return (`
 		<div>
-	 	<p>Information about this client:</p>
-	 	<p><h4>Business name: ${this.business_name}</h4></p>
-	 	<p><h4>Website: ${this.website}</h4></p>
-		<p><h4>Email: ${this.email}</h4></p>
-		<p><h4>Projects: ${projectData(this)}</h4></p>
-		<p><h4>Address: ${this.address}<h4></p>
+	 	<p><h4>Client information:</p></h4>
+	 	<p><h5></p>Business name: ${this.business_name}</h5></p></p>
+	 	<p><h5></p>Website: ${this.website}</h5></p></p>
+		<p><h5></p>Email: ${this.email}</h5></p></p>
+		<p><h5></p>Projects: ${projectData(this)}</h5></p></p>
+		<p><h5></p>Address: ${this.address}<h5></p></p>
 		<div class="mapouter">
 		<div class="gmap_canvas">
 		<iframe width="399" height="271" id="gmap_canvas" src="https://maps.google.com/maps?q=${this.address}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>Werbung: <a href="https://www.jetzt-drucken-lassen.de">jetzt-drucken-lassen.de</a></div><style>.mapouter{position:relative;text-align:right;height:271px;width:399px;}.gmap_canvas {overflow:hidden;background:none!important;height:271px;width:399px;}</style>
 		</div>
 		</div>
+		<br>
+		<button id="hide" class="btn btn-primary btn-md">CLOSE</button>
 		`)
 }
+
+
 
 function listenForNewClientFormClick(){
 		$('button.js-new').on('click', function(e){
@@ -82,8 +117,6 @@ function listenForNewClientFormClick(){
 			let newClientForm = Client.newClientForm();
 			let place = document.getElementById("new-client")
 			place.innerHTML = newClientForm
-
-				console.log('new form')
 				$('#newc').on('submit', function(e){
 				e.preventDefault();
 				console.log(e)
@@ -99,7 +132,7 @@ function listenForNewClientFormClick(){
 			
 	});
 	
-		$('#new_client').on('submit', function(e){
+	$('#new-client').on('submit', function(e){
 	e.preventDefault();
 	const values = $(this).serialize()
 	$.post('/clients', values)
