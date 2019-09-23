285eaa839a7dde4bfab68872fbf71f55d8d0256b
VBTX <jv.svedaite@gmail.com>
2019-09-23 17:30:45 -0500

    add function to calculate total automatically

--- app/assets/javascripts/invoices.js
+++ app/assets/javascripts/invoices.js
@@ -1,3 +1,25 @@
-$(document).ready(()=>{
-    console.log('Here I am in invoices')
+$(document).ready(() => {
+	console.log('Here I am in invoices');
+	listenForInput();
 });
+
+function listenForInput() {
+	let typingTimer;
+	let doneTypingInterval = 2000;
+
+	//on keyup, start the countdown
+	$('#invoice_amount').keyup(function() {
+		clearTimeout(typingTimer);
+		if ($('#invoice_amount').val()) {
+			typingTimer = setTimeout(doneTyping, doneTypingInterval);
+		}
+	});
+
+	//user is "finished typing" calculate
+	function doneTyping() {
+		let invoiceRate = document.getElementById('invoice_rate').value;
+		let invoiceQuantity = document.getElementById('invoice_amount').value;
+		let total = document.getElementById('invoice_total');
+		total.value = invoiceRate * invoiceQuantity;
+	}
+}
