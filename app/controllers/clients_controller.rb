2dd4173cb1c76cf80cf59bbc1e78af958cab15f0
VBTX <jv.svedaite@gmail.com>
2019-09-27 15:21:19 -0500

    leave only JS search function and delete one in the controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,27 +2,12 @@ class ClientsController < ApplicationController
 	before_action :current_user
 	
 	def index
-		if params[:business_name] && Client.all.find_by(business_name: params[:business_name])#if user is using SEARCH function
-			@client = Client.with_business_name(params[:business_name])
- 			@client = @client.first #I'm using scope so it returns Active Record Relation array, therefore I need to select FIRST
- 			render :show
- 		# renders show page when prompted by search
- 		elsif params[:business_name]
- 		flash.now[:notice] = "No results"
- 		@clients = current_user.clients
- 		respond_to do |format|
- 			format.html {render :index}
- 			format.json {render json: @clients}
- 		end
- 		else
  			@clients = current_user.clients
  			respond_to do |format|
  			format.html {render :index}
  			format.json {render json: @clients}
  		end
  	end
-
-	end
 
 	def new
 		@client = Client.new
--- app/models/client.rb
+++ app/models/client.rb
@@ -4,5 +4,4 @@ class Client < ApplicationRecord
 	has_many :invoices, through: :projects
 	validates_presence_of :business_name
 	validates :business_name, :uniqueness => {:scope=>:user_id}
-	scope :with_business_name, lambda { |name| where(business_name: name) }
 end
