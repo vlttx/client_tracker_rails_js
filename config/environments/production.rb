5b64d70f4ab06999e3cc9e1060979c6ac95ea181
VBTX <jv.svedaite@gmail.com>
2019-10-02 18:25:59 -0500

    config uglifier in order to use es6 syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
