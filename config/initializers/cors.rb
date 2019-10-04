dd30b325c867aad08d2d76f362ead07a2434a402
VBTX <jv.svedaite@gmail.com>
2019-10-04 10:31:38 -0500

    change cors facebook login troubleshooting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://blooming-sea-43662.herokuapp.com'
 
     resource '*',
       headers: :any,
