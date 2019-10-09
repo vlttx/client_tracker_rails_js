b052ad2971f67013502710e9630fd844e22791c0
VBTX <jv.svedaite@gmail.com>
2019-10-09 17:47:37 -0500

    add diff option for client rendering when saving

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -30,7 +30,11 @@ class ClientsController < ApplicationController
 	def create
 		@client = current_user.clients.build(client_params)
 		if @client.save
-			render json: @client
+			# render json: @client
+			respond_to do |format|
+			format.html {render:show}
+			format.json {render json: @client}
+			end
 		else
 			redirect_to new_client_path
 		end
